436fcb2faa1448d3eefdc27e73a99cb41bc96a16
prafulgupta84 <prafulgupta84@gmail.com>
2018-04-04 18:11:37 +0530

    Update compilePython.groovy

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -4,7 +4,3 @@ def call (String directory = '.') {
     echo("Compiling ${directory}")
     compileDirectory(this, directory)
 }
-
-#def call(String directory = '.') {  
-#  sh """find ${directory} -name '*.py' -print0 |  xargs -0 -t -L 1 python2 -m py_compile"""
-#}
