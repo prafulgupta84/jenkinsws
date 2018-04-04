8c800a86ab5fbad24ecf3bd727865fd1d0cbb92c
prafulgupta84 <prafulgupta84@gmail.com>
2018-04-04 17:21:44 +0530

    Update compilePython.groovy

--- vars/compilePython.groovy
+++ vars/compilePython.groovy
@@ -1,7 +1,3 @@
-def call() {  
-  if (fileExists("requirements.txt")) {
-    sh "virtualenv venv"
-    sh "venv/bin/pip install -r requirements.txt" 
-  }  
-  sh """find . -name '*.py' -print0 |  xargs -0 -t -L 1 python2 -m py_compile"""
+def call(String directory = '.') {  
+  sh """find ${directory} -name '*.py' -print0 |  xargs -0 -t -L 1 python2 -m py_compile"""
 }
