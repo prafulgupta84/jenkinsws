28fd4c520743e291afdc717308afcb61181242db
prafulgupta84 <prafulgupta84@gmail.com>
2018-04-04 18:12:50 +0530

    Update PythonCompiler.groovy

--- src/eu/inuits/PythonCompiler.groovy
+++ src/eu/inuits/PythonCompiler.groovy
@@ -1,6 +1,6 @@
 package eu.inuits
 class PythonCompiler {
-static def compilerDirectory(script, directory)	{
+static def compileDirectory(script, directory)	{
 script.sh """find ${directory} -name '*.py' \
  -print0 | xargs -0 -t -L 1 python -m py_compile"""
 }
